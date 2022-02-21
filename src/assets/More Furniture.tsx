366bc39ef0c7256ad9d55ded8411dde576045d0d
Daniel Reimer <daniel.reimer@reactos.org>
2022-02-21 22:03:39 +0100

    Moar new stuff

---
+++ src/assets/More Furniture.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="More Furniture" tilewidth="32" tileheight="32" tilecount="1024" columns="16">
+ <image source="furniture.png" width="512" height="2048"/>
+</tileset>
