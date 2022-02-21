79c68bb5b95c486b614ebcf2d8df7dd0458892ff
Daniel Reimer <daniel.reimer@reactos.org>
2022-02-21 22:32:09 +0100

    Fixes

--- src/assets/DrinksandFood.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="DrinksandFood" tilewidth="32" tileheight="32" tilecount="1024" columns="16">
- <image source="food_and_drinks.png" width="512" height="2048"/>
-</tileset>
