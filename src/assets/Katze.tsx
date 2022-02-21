d9efccdbc85b2b77fa3d0eac50e1664982e0910c
Daniel Reimer <daniel.reimer@reactos.org>
2022-02-21 22:16:35 +0100

    Enough today...

---
+++ src/assets/Katze.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Katze" tilewidth="32" tileheight="32" tilecount="3" columns="3">
+ <image source="cat.png" width="96" height="32"/>
+</tileset>
