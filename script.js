0b3ac06d74a05a2f097351af5e456e39d3eb7099
Daniel Reimer <daniel.reimer@reactos.org>
2025-10-30 22:38:36 +0100

    Another day, another test

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-let currentPopup: any = undefined;
+let RIPPopup;
+let clockPopup;
 
 WA.room.onEnterLayer('clockZone').subscribe(() => {
     const today = new Date();
     const time = today.getHours() + ":" + today.getMinutes();
-    currentPopup = WA.ui.openPopup("clockPopup","It's " + time,[]);
+    clockPopup = WA.ui.openPopup("popupRectangle","It's " + time,[{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    ]);
 })
 
-WA.room.onLeaveLayer('clockZone').subscribe(closePopUp)
+WA.room.onLeaveLayer('clockZone').subscribe(() => {
+    clockPopup.close();
+})
 
 WA.room.onEnterLayer('RIPZone').subscribe(() => {
-    currentPopup = WA.ui.openPopup("RIPPopup","R.I.P.\nGé van Geldorp\n✝ 2011\nStefan Ginsberg\n✝ 2022\nJames Tabor\n✝ 2023\nWe miss you, guys...",[]);
+    RIPPopup = WA.ui.openPopup("popupRectangle","R.I.P.\nGé van Geldorp\n✝ 2011\nStefan Ginsberg\n✝ 2022\nJames Tabor\n✝ 2023\nWe miss you, guys...",[{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    ]);
 })
 
-WA.room.onLeaveLayer('RIPZone').subscribe(closePopUp)
-
-function closePopUp(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
+WA.room.onLeaveLayer('RIPZone').subscribe(() => {
+    RIPPopup.close();
+})
