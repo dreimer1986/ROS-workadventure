901c4c94791d8623151e4b7dc6784f89dc62d113
Daniel Reimer <daniel.reimer@reactos.org>
2025-11-23 17:30:10 +0100

    Revert party

--- src/index.ts
+++ src/index.ts
@@ -5,37 +5,25 @@ import {bootstrapExtra} from "@workadventure/scripting-api-extra";
 // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
 bootstrapExtra().catch(e => console.error(e));
 
-let RIPPopup;
-let clockPopup;
+let currentPopup: any = undefined;
 
 WA.room.onEnterLayer('clockZone').subscribe(() => {
     const today = new Date();
     const time = today.getHours() + ":" + today.getMinutes();
-    clockPopup = WA.ui.openPopup("popupRectangle","It's " + time,[{
-        label: "Close",
-        className: "primary",
-        callback: (popup) => {
-            // Close the popup when the "Close" button is pressed.
-            popup.close();
-        }
-    ]),
+    currentPopup = WA.ui.openPopup("clockPopup","It's " + time,[]);
 })
 
-WA.room.onLeaveLayer('clockZone').subscribe(() => {
-    clockPopup.close();
-})
+WA.room.onLeaveLayer('clockZone').subscribe(closePopUp)
 
 WA.room.onEnterLayer('RIPZone').subscribe(() => {
-    RIPPopup = WA.ui.openPopup("popupRectangle","R.I.P.\nGé van Geldorp\n✝ 2011\nStefan Ginsberg\n✝ 2022\nJames Tabor\n✝ 2023\nWe miss you, guys...",[{
-        label: "Close",
-        className: "primary",
-        callback: (popup) => {
-            // Close the popup when the "Close" button is pressed.
-            popup.close();
-        }
-    ]),
+    currentPopup = WA.ui.openPopup("RIPPopup","R.I.P.\nGé van Geldorp\n✝ 2011\nStefan Ginsberg\n✝ 2022\nJames Tabor\n✝ 2023\nWe miss you, guys...",[]);
 })
 
-WA.room.onLeaveLayer('RIPZone').subscribe(() => {
-    RIPPopup.close();
-})
+WA.room.onLeaveLayer('RIPZone').subscribe(closePopUp)
+
+function closePopUp(){
+    if (currentPopup !== undefined) {
+        currentPopup.close();
+        currentPopup = undefined;
+    }
+}
